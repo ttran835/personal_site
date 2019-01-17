385e6c0ca370c98f12b8116f9806628b9dc0e880
tim <timothy.tran835@gmail.com>
2019-01-16 23:07:27 -0800

    completed the layout and skeleton section for about me, will move onto writng about my personal narrative

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,10 +1,12 @@
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/jquery/dist/jquery.min.js';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
 //hello
 //bootstrap;
-import 'bootstrap';
-import '../../css/index.css';
 
 render(<App />, document.getElementById('app'));
+
+im;
--- client/src/components/About/AboutMe.jsx
+++ client/src/components/About/AboutMe.jsx
@@ -6,18 +6,39 @@ import styles from '../../../../css/components/AboutMe.css';
 
 export default function About(props) {
   return (
-    <div className={`card text-center ${styles.noBorder}`}>
-      <div className={`card-block ${styles.aboutMeBg} ${styles.shadow} `}>
-        <h2 className="card-title">About Me</h2>
-        <p
-          className={` col-md-6 offset-md-3 card-text  ${
-            styles.paragraphWidth
-          }`}
-        >
-          This is where the about me section will go, for now it will just be
-          some text to fill the place of it in order to resize and making sure
-          that the information looks okay when the actual thing.
-        </p>
+    <div className={`container 0-px ${styles.noBorder}`}>
+      <div className={`container ${styles.aboutMeBg} ${styles.shadow} `}>
+        <div className="row">
+          <div className="col col-md-4">
+            <img
+              className={`img-thumbnail ${styles.aboutMeImg}`}
+              src="https://s3.amazonaws.com/personal-site-tim/48270361_10218535878854978_2309076050140725248_o.jpg"
+            />
+          </div>
+          <div className="col col-lg-8">
+            <div className={`jumbotron ${styles.jumboTronBg}`}>
+              <h3 className="display-3">Hello!</h3>
+              <h4 className="display-3">
+                I'm <strong>Tim Tran</strong>
+              </h4>
+              <p className="lead">
+                Software Engineer and advocate for financial literacy.
+              </p>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th scope="row">Email</th>
+                    <td>timsters93@gmail.com</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Phone</th>
+                    <td>1 (714) - 902 - 7622</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
         <div className={`container-fluid ${styles.aboutMeFooter}`}>
           <Footer />
         </div>
